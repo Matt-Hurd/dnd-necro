5bebb26cd30321865779c8966f23e90daa2da275
root <root@dnd-core.localdomain>
2017-02-12 01:27:46 +0000

    Improve error handling in case of stall

--- necro.py
+++ necro.py
@@ -1,22 +1,108 @@
 import digitalocean
 from secrets import DO_TOKEN
+from datetime import datetime
+from datetime import timedelta
+import time
+import thread
+import socket
 
 region = 'SFO2'
-image = '22780196' #Change to image name
+image = '22783408' #Wisp-B
 size_slug = '512mb'
-tag = digitalocean.Tag(token=access_token, name="wisp")
-tag.create()
 class Wisp:
-    def __init__(self, access_token, name):
+    active = False
+    def __init__(self, access_token, name, tag):
         self.droplet = digitalocean.Droplet(token=access_token,
                                name = name,
                                region = region,
                                image = image,
                                size_slug=size_slug,
+                               ssh_keys=[],
                                private_networking=True,
                                backups=False)
         self.droplet.create()
-        tag.add_droplets([str(self.droplet.id)])
+
+    def kill(self):
+        self.droplet.destroy()
+
+    @property
+    def created_at(self):
+        if self.droplet.created_at:
+            return datetime.strptime(self.droplet.created_at, "%Y-%m-%dT%H:%M:%SZ")
+        else:
+            self.droplet.load()
+            return None
+
+def check_alive(wisps):
+    to_remove = []
+    for wisp in wisps:
+        if not wisp.active and wisp.created_at:
+            if (datetime.now() - wisp.created_at) > timedelta(minutes = 2):
+                to_remove.append(wisp)
+    for wisp in to_remove:
+        wisps.remove(wisp)
+        wisp.kill()
+        print("Killed a wisp")
+
+def party_wipe(wisps):
+    for wisp in wisps:
+        wisp.kill()
+
+def receive_alive_signal(wisps, ip):
+    for wisp in wisps:
+        if not wisp.droplet.private_ip_address:
+            wisp.droplet.load()
+            if not wisp.droplet.private_ip_address:
+                print("Node isn't loaded yet?")
+                continue
+        if not wisp.active:
+            if ip == wisp.droplet.private_ip_address:
+                print("Node Loaded")
+                wisp.active = True
+
+def udp_listener(wisps):
+    UDP_IP = "127.0.0.1"
+    UDP_PORT = 5005
+
+    sock = socket.socket(socket.AF_INET, # Internet
+                         socket.SOCK_DGRAM) # UDP
+    sock.bind(('', UDP_PORT))
+
+    while True:
+        data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+        if data == 'WHISPER':
+            print("%s is alive" % addr[0])
+            receive_alive_signal(wisps, addr[0])
+
+def check_thread(wisps):
+    while True:
+        check_alive(wisps)
+        time.sleep(10)
+
+def clean_by_tag(tag):
+    manager = digitalocean.Manager(token=DO_TOKEN)
+    my_droplets = manager.get_all_droplets(tag_name=tag)
+    for droplet in my_droplets:
+        droplet.destroy()
 
 if __name__ == '__main__':
-	wisp = Wisp(DO_TOKEN, 'wisp-from-api')
+    clean_by_tag('wisp')
+    wisps = []
+    tag = digitalocean.Tag(token=DO_TOKEN, name="wisp")
+    tag.create()
+    try:
+        manager = digitalocean.Manager(token=DO_TOKEN)
+        count = len(manager.get_all_droplets())
+        while count < 10:
+            wisps.append(Wisp(DO_TOKEN, 'wisp-from-api-%d' % count, tag))
+            count += 1
+        tag.add_droplets([str(w.droplet.id) for w in wisps])
+    except Exception as e:
+        print(e)
+        print("Waiting before killing all nodes")
+        time.sleep(20)
+        party_wipe(wisps)
+    thread.start_new_thread(check_thread, (wisps,))
+    thread.start_new_thread(udp_listener, (wisps,))
+    while True:
+        pass
